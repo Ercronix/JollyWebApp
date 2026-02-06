3e953c60559cd28afe5482e696325d12c8263538
mornhinweg <mornhinweg@stud.hs-heilbronn.de>
2026-02-06 05:46:30 +0100

    design changes

--- frontend/src/presentation/pages/GamePage/GamePage.tsx
+++ frontend/src/presentation/pages/GamePage/GamePage.tsx
@@ -343,11 +343,16 @@ export function GamePage() {
             <MainLayout>
                 <div className="relative z-10 container mx-auto px-4 py-6">
                     <div className="flex flex-col gap-3 max-w-4xl mx-auto">
-                        <div className="flex flex-col items-center gap-2 justify-center w-full">
+                        <div className="flex flex-col items-center gap-2 w-full">
                         {searchParams.accessCode && (
+                            <div className="flex items-center">
+                            <svg className="w-5 h-5 text-purple-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+                            </svg>
                             <Text as="h1" size="sm" weight="bold" className="bg-gradient-to-r from-purple-400 via-white-300 to-blue-400 bg-clip-text text-transparent text-center whitespace-nowrap">
-                                (private Lobby)
+                                private Lobby
                             </Text>
+                            </div>
                         )}
                         <GameHeader lobbyName={searchParams.lobbyName}/>
                     </div>
